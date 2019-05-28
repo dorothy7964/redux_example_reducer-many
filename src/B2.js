728c5597c663f218f179428b48f4ecf40d64e347
SEC\user <dorothy_007@naver.com>
2019-05-28 18:37:03 +0900

    connect (데이터, 액션 연결)
    리듀서와 컴포넌트 연결 (mapStateToProps, mapDispatchToProps)

--- src/B2.js
+++ src/B2.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { increament, decreament } from './actions/counting';
 import B3 from './B3';
 
-const B2 = ({btn}) => {
+const B2 = ({btn, close, text, onIncreament, onDecreament}) => {
   return (
     <div>
       <h2>B2</h2>
+      <div>스토어로 부터 가지고 온 데이터는 : {close}</div>
+      <div>스토어로 부터 가지고 온 두번째 데이터는 : {text}</div>
+      <button onClick={()=>{onIncreament(20)}}>스토어에 이벤트(액션) + 전달하기</button>
+      <button onClick={()=>{onDecreament(10)}}>스토어에 이벤트(액션) - 전달하기</button>
 
       <B3 btn={btn} />
     </div>
   )
 }
 
-export default B2;
+const mapStateToProps = (state) => {
+  return {
+    close : state.number.count,
+    text : state.title.title
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncreament : (num) => { dispatch(increament(num)) },
+    onDecreament : (num) => { dispatch(decreament(num)) }
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(B2);
