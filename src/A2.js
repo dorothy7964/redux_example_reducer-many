c70a4ed90fbb5571865db07949838d6792b29ecc
SEC\user <dorothy_007@naver.com>
2019-05-28 18:33:16 +0900

    connect (데이터 연결)
    리듀서와 컴포넌트 연결 (mapStateToProps, null)

--- src/A2.js
+++ src/A2.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import A3 from './A3';
 
-const A2 = ({num}) => {
+const A2 = ({num, open, text}) => {
   return (
     <div>
       <h2>A2</h2>
       <div>{num}</div>
+      <div>스토어로 부터 가지고 온 데이터는 : {open}</div>
+      <div>스토어로 부터 가지고 온 두번째 데이터는 : {text}</div>
 
       <A3 num={num} />
     </div>
   )
 }
 
-export default A2;
+const mapStateToProps = (state) => {
+  return {
+    open : state.number.count,
+    text : state.title.title
+  }
+}
+
+export default connect(mapStateToProps,null)(A2);
